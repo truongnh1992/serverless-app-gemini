dba2e3c587275cff90e297b8a9d1aa1b466f0e32
Alex Nguyen <truong@clare.ai>
2024-11-04 23:37:52 +0700

    Update

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+	fmt.Fprintf(w, "Hello, Google Developer Group Vientiane 🇱🇦\n")
+	fmt.Fprintf(w, "Version: 1.0.0\n")
+}
